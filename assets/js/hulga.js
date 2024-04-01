1a949e673fc65a22271be3fc0c0487d21b2dd53e
zilcH40 <wlh233@live.com>
2024-04-01 23:23:29 +0800

    fix: more proper way to change link color

--- assets/js/hulga.js
+++ assets/js/hulga.js
@@ -48,9 +48,5 @@ document.addEventListener('DOMContentLoaded', () => {
 // activate back to top
 addBackToTop({
   diameter: 42,
-// {{ if .Site.Params.primaryColor }}
-//   backgroundColor: "{{ .Param "primaryColor" }}",
-// {{ end }}
   innerHTML: '<svg viewBox="0 0 24 24"><path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"/></svg>',
-  textColor: '#fff'
 });
